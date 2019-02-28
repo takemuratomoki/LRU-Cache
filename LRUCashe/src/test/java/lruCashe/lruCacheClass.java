11df778f170685de773e2f05a02876156131bb09
takemuratomoki <c611701554@tokushima-u.ac.jp>
2019-02-28 20:39:34 +0900

    cacheの綴りを間違えていたので直せる範囲で訂正

---
+++ LRUCashe/src/test/java/lruCashe/lruCacheClass.java
@@ -0,0 +1,9 @@
+//一定の数に達したら使われていない順に要素が削除されていくMapのような入れ物を作りたい
+//このためLastRecentlyUsedCacheを作成する。
+//キャッシュの最大サイズに達した時に、最も使われていないデータから順に消される仕組みが欲しい
+
+package lruCashe;
+
+public class lruCacheClass {
+
+}
