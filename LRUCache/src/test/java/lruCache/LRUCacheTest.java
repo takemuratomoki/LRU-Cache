125e866c23a730a97067cd2a463db6f8ff018d4f
takemuratomoki <c611701554@tokushima-u.ac.jp>
2019-03-02 03:32:59 +0900

    checkNullNum関数を外部クラスからテストするための関数（testCNNForKey,testCNNForData）を呼び出しcheckNullNum関数のテストを実装、テストのパスを確認

--- LRUCache/src/test/java/lruCache/LRUCacheTest.java
+++ LRUCache/src/test/java/lruCache/LRUCacheTest.java
@@ -12,7 +12,7 @@ public class LRUCacheTest {
 	@Test
 	public void _0番目の要素に格納するput関数テスト(){
 		try {
-//			lru.put("hoge1", "hogehoge1");
+			lru.put("hoge1", "hogehoge1");
 		}catch(Exception e) {
 			fail(e.getMessage());
 		}
@@ -23,4 +23,25 @@ public class LRUCacheTest {
 		assertThat(lru.get("hoge1"),is("1"));
 	}
 	
+	@Test
+	public void 何も格納していない時のkey配列のnullチェック() {
+		assertThat(lru.testCNNForKey(),is(0));
+	}
+	
+	@Test
+	public void 何も格納していない時のdata配列のnullチェック() {
+		assertThat(lru.testCNNForData(),is(0));
+	}
+	
+	@Test
+	public void _1つだけ要素を格納した時のkey配列のnullチェック() {
+		lru.put("hoge1", "hogehoge1");
+		assertThat(lru.testCNNForKey(),is(1));
+	}
+	
+	@Test
+	public void _1つだけ要素を格納した時のdata配列のnullチェック() {
+		lru.put("hoge1", "hogehoge1");
+		assertThat(lru.testCNNForData(),is(1));
+	}
 }
