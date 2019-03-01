feaf52145a7c6512a9faaa99c68a7fcf98458e51
takemuratomoki <c611701554@tokushima-u.ac.jp>
2019-03-01 19:23:38 +0900

    TODOの順序更新とLRUCacheTest.javaの追加

---
+++ LRUCashe/src/test/java/lruCashe/LRUCacheTest.java
@@ -0,0 +1,14 @@
+package lruCashe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LRUCacheTest {
+
+	@Test
+	public void test() {
+		fail("‚Ü‚¾ŽÀ‘•‚³‚ê‚Ä‚¢‚Ü‚¹‚ñ");
+	}
+
+}
