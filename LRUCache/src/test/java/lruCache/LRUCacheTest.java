514dd490afc85f0d6ed2553a8f81a9a88ab4d722
takemuratomoki <c611701554@tokushima-u.ac.jp>
2019-03-02 23:32:32 +0900

    put関数のテスト5つ実装、テストクリアを確認

--- LRUCache/src/test/java/lruCache/LRUCacheTest.java
+++ LRUCache/src/test/java/lruCache/LRUCacheTest.java
@@ -23,18 +23,21 @@ public class LRUCacheTest {
 			lru = new LRUCacheClass();	
 		}
 		
+		//要素数が5の時のみ
 		@Test
 		public void get関数で正しい値が返ってくるかのテスト() {
 			lru.testGetMethod();
 			assertThat(lru.get("k1"), is("d1"));
 		}
 		
+		//要素数が5の時のみ
 		@Test
 		public void 保存されていないkey値が引数に入れられた時nullが返ってくるかのテスト() {
 			lru.testGetMethod();
 			assertThat(lru.get("testes"),nullValue());
 		}
 		
+		//要素数が5の時のみ
 		@Test
 		public void get関数で一度呼び出した値が最後尾に移動しているかテスト() {
 			lru.testGetMethod();
@@ -67,7 +70,42 @@ public class LRUCacheTest {
 			assertThat(lru.testCNNForData(),is(2));
 		}
 
-		//
+		@Test
+		public void put関数で配列の先頭に要素が保存されるテスト() {
+			lru.put("k1", "d1");
+			assertThat(lru.data[0],is("d1"));
+		}
+		
+		@Test
+		public void _2度目のput関数呼び出しで二番目に要素が保存されるかのテスト(){
+			lru.put("k1", "d1");
+			lru.put("k2", "d2");
+			assertThat(lru.data[1],is("d2"));
+		}
+		
+		@Test
+		public void _2度目のput関数呼び出しで既存のkey値が指定されたとき上書きされる() {
+			lru.put("k1", "d1");
+			lru.put("k1", "d2");
+			assertThat(lru.get("k1"),is("d2"));
+		}
+		
+		//要素数が5の時のみ
+		@Test
+		public void put関数の実行で要素が最大となる時先頭データが削除されるかのテスト(){
+			lru.testGetMethod();
+			lru.put("hoge", "hogehoge");
+			assertThat(lru.get("k1"),nullValue());
+			assertThat(lru.data[0],is("d2"));
+		}
+		
+		//要素数が5の時のみ
+		@Test
+		public void put関数の実行で要素が最大となる時最後の要素がnullになっているかのテスト() {
+			lru.testGetMethod();
+			lru.put("hoge", "hogehoge");
+			assertThat(lru.data[4],nullValue());
+		}
 		
 	}
 	
